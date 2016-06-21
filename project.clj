b38a8fa537b34ee8e5c0e2a6b24875a581b5116b
Andres Gomez Urquiza <andres@fractal.ai>
2016-06-21 15:35:46 -0500

    deps

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cheshire "5.6.1"]     ;json support
+  :dependencies [[cheshire "5.6.2"]     ;json support
                  [org.clojure/clojure "1.8.0"]
-                 [compojure "1.5.0"]    ;for predefined routes
+                 [compojure "1.5.1"]    ;for predefined routes
                  [environ "1.0.3"]      ;for env variables
                  [com.novemberain/monger "3.0.2"]]
   :repl-options {:init-ns mongerr.core})
