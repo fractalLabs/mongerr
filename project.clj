cf40113fc8b12987304b138449af84fe842e7069
Andres Gomez Urquiza <andres.gomez@datos.mx>
2017-08-23 20:11:41 -0500

    updated deps

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cheshire "5.6.3"]     ;json support
+  :dependencies [[cheshire "5.8.0"]     ;json support
                  [org.clojure/clojure "1.8.0"]
-                 [compojure "1.5.1"]    ;for predefined routes
+                 [compojure "1.6.0"]    ;for predefined routes
                  [environ "1.1.0"]      ;for env variables
                  [com.novemberain/monger "3.1.0"]
-                 [medley "0.8.3"]]
+                 [medley "1.0.0"]]
   :repl-options {:init-ns mongerr.core})
