6c66336b7d590f1f992b2630414136c11850bc65
nex <andresgu@gmail.com>
2019-03-14 20:11:18 -0600

    updated dependencies

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cheshire "5.8.0"]     ;json support
-                 [org.clojure/clojure "1.8.0"]
-                 [compojure "1.6.0"]    ;for predefined routes
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [cheshire "5.8.1"]     ;json support
+                 [compojure "1.6.1"]    ;for predefined routes
                  [comun "0.1.0-SNAPSHOT"]
                  [digitalize "0.1.0-SNAPSHOT"]
                  [environ "1.1.0"]      ;for env variables
-                 [com.novemberain/monger "3.1.0"]
-                 [medley "1.0.0"]]
+                 [com.novemberain/monger "3.5.0"]
+                 [medley "1.1.0"]]
   :repl-options {:init-ns mongerr.core})
